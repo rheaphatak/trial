6ec79127871aae1b42ad3221c7d15274744e1524
rheaphatak <100486743+rheaphatak@users.noreply.github.com>
2022-10-08 17:45:55 -0500

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,37 @@
+
+from PIL import Image
+from pytesseract import pytesseract
+
+
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
+import re
+import pytesseract
+from pytesseract import Output
+
+import streamlit as st
+
+st.title('Hi pls work!')
+
+
+
+uploaded_file = st.file_uploader("Choose a file")
+
+
+if uploaded_file is not None:
+    img = cv2.imread(uploaded_file.name)
+
+    d = pytesseract.image_to_data(img, output_type=Output.DICT)
+
+
+    conversation =''
+    n_boxes = len(d['level'])
+    for i in range(n_boxes):
+        conversation += d['text'][i] + " "
+        (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    st.write("conversation:" + conversation)
+    cv2.imshow(img)
+    cv2.waitKey(0)
