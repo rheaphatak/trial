0bb45b27b237401b927d77616d80ee3bf2297217
rheaphatak <100486743+rheaphatak@users.noreply.github.com>
2022-10-09 09:38:43 -0500

    Update temp.py

--- temp.py
+++ temp.py
@@ -21,7 +21,7 @@ uploaded_file = st.file_uploader("Please choose an image file")
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image)
-    st.write('HEYY:')
+    st.write('Uploaded Image:')
     st.write(uploaded_file.name)
     #img = cv2.imread(uploaded_file)
     saved_img = image.save(uploaded_file.name)
@@ -33,7 +33,10 @@ if uploaded_file is not None:
     conversation =''
     n_boxes = len(d['level'])
     for i in range(n_boxes):
+        
         conversation += d['text'][i] + " "
+        if ((d[text][i][-1] == '.') | (d[text][i][-1] == '!') | (d[text][i][-1] == '?')):
+            coversation += '\n'
         (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
