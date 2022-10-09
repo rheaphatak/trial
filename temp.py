25b43162aad0ecec1690b11f81009b05dada56e5
rheaphatak <100486743+rheaphatak@users.noreply.github.com>
2022-10-08 19:15:51 -0500

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,12 +1,12 @@
 #from PIL import Image
 #from pytesseract import pytesseract
 
-#import cv2
-#from matplotlib import pyplot as plt
-#import numpy as np
-#import re
-#import pytesseract
-#from pytesseract import Output
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
+import re
+import pytesseract
+from pytesseract import Output
 
 
 import streamlit as st
