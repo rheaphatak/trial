2730bb6141fc841aec624fc11873becfd6924d28
rheaphatak <100486743+rheaphatak@users.noreply.github.com>
2022-10-09 09:44:49 -0500

    Update temp.py

--- temp.py
+++ temp.py
@@ -33,10 +33,11 @@ if uploaded_file is not None:
     conversation =''
     n_boxes = len(d['level'])
     for i in range(n_boxes):
-        st.write('word: ' + d['text'][i] + '\n')
+        #st.write('word: ' + d['text'][i] + '\n')
         conversation += d['text'][i] + " "
-        #if ((d['text'][i][-1] == '.') | (d['text'][i][-1] == '!') | (d['text'][i][-1] == '?')):
-            #coversation += '\n'
+        if ((d['text'][i][-1]):
+            if ((d['text'][i][-1] == '.') | (d['text'][i][-1] == '!') | (d['text'][i][-1] == '?')):
+                coversation += '\n'
         (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
