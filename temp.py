f77337f67af3d50741fef8d5647a4ada887e5926
rheaphatak <100486743+rheaphatak@users.noreply.github.com>
2022-10-08 20:22:27 -0500

    Update temp.py

--- temp.py
+++ temp.py
@@ -31,7 +31,7 @@ if uploaded_file is not None:
     for i in range(n_boxes):
         conversation += d['text'][i] + " "
         (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     st.write("conversation:" + conversation)
     #cv2.imshow(img)
